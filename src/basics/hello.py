baed5633365612610ffceb381696fe892457fc89
jibo4786 <jibo4786@gmail.com>
2025-09-19 16:36:24 +0200

    Den 1: první skript - vstup, výstup, aritmetika

---
+++ src/basics/hello.py
@@ -0,0 +1,14 @@
+jmeno = input("Zadej své jméno: ")
+
+print(f"Ahoj, {jmeno}! Vítej v projektu OnlyPython.")
+
+a = 10
+b = 3
+
+print("Součet: ", a+b)
+print("Rozdíl: ", a-b)
+print("Násobek: ", a*b)
+print("Podíl: ", a/b)
+print("Celé dělení: ", a//b)
+print("Zbytek po dělení: ", a%b)
+print("Umocňování: ", a**b)
